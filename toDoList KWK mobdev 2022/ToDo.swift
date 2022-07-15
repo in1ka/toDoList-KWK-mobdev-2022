73ea0cad0bb6350108247b6bf6cfcf57d2a1eebc
Inika B <inikamehro@gmail.com>
2022-07-15 16:25:30 -0700

    Iteration 0 complete. WOOOHOHOHOHOHOHO

---
+++ toDoList KWK mobdev 2022/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDoList KWK mobdev 2022
+//
+//  Created by Scholar on 7/15/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = "OH NO IT'S BROKEN!!"
+    var important = true
+    
+}
